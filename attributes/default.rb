814fa7bd5a048dcd4ccea019eb1c7d7b981df249
Jonathan Poole <jpoole@digitaljedi.ca>
2014-04-20 08:28:21 -0400

    Adjusting cookbook to import database

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:wp_cookbook][:user]      = 'vagrant'
 default[:wp_cookbook][:hostname]  = 'auvik.com'
-default[:wp_cookbook][:dir]       = '/srv/www/auvik.com'
+default[:wp_cookbook][:dir]       = '/srv/www/auvik.com/web'
 default[:wp_cookbook][:wp_cli]    = 'vendor/bin/wp'
 default[:wp_cookbook][:theme_dir] = 'web/app/themes/'
 default[:wp_cookbook][:db_name]   = 'auvik.com'
@@ -10,5 +10,5 @@ default[:wp_cookbook][:wp_admin_user]  = 'admin'
 default[:wp_cookbook][:wp_admin_pass]  = 'admin'
 default[:wp_cookbook][:wp_admin_email] = 'admin@auvik.com'
 
-default[:wp_cookbook][:wp_import] = false
-default[:wp_cookbook][:wp_import_dump] = 'prod.sql'
+default[:wp_cookbook][:wp_import] = true
+default[:wp_cookbook][:wp_import_dump] = '/srv/www/auvik.com/web/wp_dev.sql'
