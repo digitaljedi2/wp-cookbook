b5e2652d225fc0c9c04d5c80a7aa2dc9743dcca6
Jonathan Poole <jpoole@auvik.com>
2014-06-09 10:26:26 -0400

    Changing default user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:wp_cookbook][:user]      = 'vagrant'
+default[:wp_cookbook][:user]      = 'auvik'
 default[:wp_cookbook][:hostname]  = 'auvik-wp'
 default[:wp_cookbook][:dir]       = '/srv/www/auvik.com/'
 default[:wp_cookbook][:wp_cli]    = 'vendor/bin/wp'
