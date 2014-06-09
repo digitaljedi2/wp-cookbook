1701e3a024f8fed9d6bcf24e6cd95dcb20b870b6
Jonathan Poole <jpoole@auvik.com>
2014-06-09 12:26:15 -0400

    Increased version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'scott.walkinshaw@gmail.com'
 license          'MIT'
 description      'Configures a webserver for WP sites'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.1'
 
 recipe 'wp::default', 'Base server setup'
 recipe 'wp::setup',   'Setup and deploy WP for dev VM'
