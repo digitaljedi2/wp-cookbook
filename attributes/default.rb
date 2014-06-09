4ece3dc3d17dcd1fd30be69009b7a8b294ebbff9
Jonathan Poole <jpoole@auvik.com>
2014-06-09 12:23:30 -0400

    Added default dump file

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,12 +3,12 @@ default[:wp_cookbook][:hostname]  = 'auvik-wp'
 default[:wp_cookbook][:dir]       = '/srv/www/auvik.com/'
 default[:wp_cookbook][:wp_cli]    = 'vendor/bin/wp'
 default[:wp_cookbook][:theme_dir] = 'web/app/themes/'
-default[:wp_cookbook][:db_name]   = 'auvik.com'
+default[:wp_cookbook][:db_name]   = 'auviki_wordpress'
 
 default[:wp_cookbook][:wp_title]       = 'Auvik Networks'
 default[:wp_cookbook][:wp_admin_user]  = 'admin'
 default[:wp_cookbook][:wp_admin_pass]  = 'admin'
 default[:wp_cookbook][:wp_admin_email] = 'admin@auvik.com'
 
-default[:wp_cookbook][:wp_import] = false
-default[:wp_cookbook][:wp_import_dump] = 'wp_dev.sql'
+default[:wp_cookbook][:wp_import] = true
+default[:wp_cookbook][:wp_import_dump] = 'wordpress.sql'
